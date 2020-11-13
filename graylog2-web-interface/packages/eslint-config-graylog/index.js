f7e745c6b5cc76e34716f8f644fe1eb9923dc29a
Dennis Oelkers <dennis@graylog.com>
2020-11-13 10:26:45 +0100

    Replacing core rule with typescript extension for `.ts(x)`.

--- graylog2-web-interface/packages/eslint-config-graylog/index.js
+++ graylog2-web-interface/packages/eslint-config-graylog/index.js
@@ -18,6 +18,9 @@ module.exports = {
       plugins: ['@typescript-eslint/eslint-plugin'],
       rules: {
         'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
       },
     },
   ],
